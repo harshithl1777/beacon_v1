e6a685b0b98190ef84abcf8fbc4c83873bc5bf88
VishnudevP <62526318+VishnudevP@users.noreply.github.com>
2021-03-07 14:41:19 -0500

    Landing Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import NavBar from './navBar/navBar';
+import Landing from './Landing/Landing'
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div>App</div>
+            <div>           
+                <NavBar/>
+                <Landing/>
+            </div>
         );
     }
 }
