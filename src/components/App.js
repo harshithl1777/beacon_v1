14eb5c9379178db4e527a257bea1a0a1256fd627
YashKapoor1004 <yashkapoor1004@gmail.com>
2021-03-09 20:43:02 -0500

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+<<<<<<< Updated upstream
 import NavBar from './NavBar/navBar';
 import Landing from './LandingContent/LandingContent'
 import CenterContent from './CenterContent/CenterContent'
 import RightContent from './RightContent/RightContent'
+=======
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
+import Landing from './LandingContent/LandingContent';
+import SignUp from './Signup';
+
+>>>>>>> Stashed changes
 import './App.css';
 import LeftContent from './LeftContent/LeftContent';
 import LastContent from './LastContent/LastContent';
@@ -11,6 +19,7 @@ import FooterContent from './FooterContent/FooterContent';
 class App extends Component {
     render() {
         return (
+<<<<<<< Updated upstream
             <div>           
                 <NavBar/>
                 <Landing/>
@@ -23,6 +32,16 @@ class App extends Component {
                 <LeftContent/>
                 <LastContent/>
                 <FooterContent/>
+=======
+            <div className="App">
+                <Router>
+                    <Switch>    
+                        <NavBar/>
+                        <Landing/>
+                        <Route path='/signup' render={props => <SignUp {...props}/>}/>
+                    </Switch>    
+                </Router>    
+>>>>>>> Stashed changes
             </div>
         );
     }
