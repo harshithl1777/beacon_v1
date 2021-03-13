175c188fb6472049776c1dc2658392c4afde7df4
YashKapoor1004 <yashkapoor1004@gmail.com>
2021-03-13 17:03:28 -0500

    App

--- src/app/App.js
+++ src/app/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import NavBar from './NavBar/navBar';
+import NavBar from './navBar/navBar';
 import Landing from './LandingContent/LandingContent'
 import CenterContent from './CenterContent/CenterContent'
 import RightContent from './RightContent/RightContent'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SignUp from './Signup/SignUp';
 import './App.css';
 import LeftContent from './LeftContent/LeftContent';
 import LastContent from './LastContent/LastContent';
 import FooterContent from './FooterContent/FooterContent';
+import LogIn from './Login/LogIn';
+import Contribute from './Contribute/Contribute';
 
 class App extends Component {
     render() {
         return (
-            <div>           
-                <NavBar/>
-                <Landing/>
-                <CenterContent/>
-                <RightContent/>
-                <LeftContent/>
-                <RightContent/>
-                <CenterContent/>
-                <RightContent/>
-                <LeftContent/>
-                <LastContent/>
-                <FooterContent/>
+            <div className="App">   
+                <Router>
+                    <Switch>           
+                        <Route exact path='/' component={NavBar}/>
+                        <Route exact path='/login' component={LogIn}/>
+                        <Route exact path='/contribute' component={Contribute}/>
+                        <Route path='/signup' component={SignUp}/>
+                    </Switch>   
+                </Router>  
             </div>
         );
     }
