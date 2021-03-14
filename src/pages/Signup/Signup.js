9f739a1fbb7118fa0a608b371b0dc4c29c99f2b7
Harshith Latchupatula <harshithl1777@gmail.com>
2021-03-13 22:13:34 -0500

    created server side and modified build script

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import logo from "./assets/beacon-logo.svg";
+import lineImg from './assets/seperator.svg';
 import './signup.css';
 
 class Signup extends Component {
@@ -12,17 +13,15 @@ class Signup extends Component {
                     <img className="logo" src={logo} alt="logo"></img>
                     <h1 className="title">Sign Up For Beacon</h1>
                     <form className="form">
-                        <input className="form__email" type="email" placeholder="Enter your email address..."/>
-                        <input className="form__pass" type="password" placeholder="Enter a password"/>
-                        <input className="form__confPass" type="password" placeholder="Confirm your password"/>
+                        <input className="signup-form__email" type="email" placeholder="Enter your email address"/>
+                        <input className="signup-form__pass" type="password" placeholder="Enter a password"/>
+                        <input className="signup-form__confPass" type="password" placeholder="Confirm your password"/>
                         <Link to={{ pathname: '/login'}}>
                             <h1 className="loginDirect">Already have an account? Log in.</h1>
                         </Link>
-                        <button className="form__getStarted" type="submit">Get Started with Beacon</button>
+                        <button className="signup-form__getStarted" type="submit">Get Started with Beacon</button>
                     </form>
-                    <div className="line1"></div>
-                        <h1 className="or">OR</h1>
-                    <div className="line2"></div>
+                    <img className='line-separator' src={lineImg} alt='Line' draggable='false' />
                 </div>
             </div>
         );
