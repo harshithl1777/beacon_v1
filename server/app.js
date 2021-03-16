7056d129b22513f83e2f1381bfcddb9921ef56d1
Harshith Latchupatula <harshithl1777@gmail.com>
2021-03-15 22:09:40 -0400

    setup auth, new user route

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: './config/.env' });
+const path = require('path');
+const auth = require('./routes/users');
+
+// mount required routes and parsing libs
+app.use(bodyParser.json());
+app.use(auth);
+
+// mount build files to server
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static('public'));
+
+// hand all unused routes to react router
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(3000 || process.env.PORT);
