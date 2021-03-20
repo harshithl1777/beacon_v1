0e660f2504ebdda2b39f64708a74920f0a353314
YashKapoor1004 <yashkapoor1004@gmail.com>
2021-03-19 21:00:35 -0400

    No Results Component

--- src/components/NoResults/NoResults.js
+++ src/components/NoResults/NoResults.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './noresults.css';
+import ex from './assets/ex.svg';
+
+
 
 class NoResults extends Component {
     render() {
         return (
-            <div>
+            <div className="no-results">
                 <div className="top-dark"></div>
-                <img className="ex" src=""></img>
-                <h1 className="no-results__title">No results found based on your search</h1>
+                <img className="ex" src={ex}></img>
+                <h1 className="no-results__title">No results found based on your search.</h1>
+                <h1 className="no-results__text">Perhaps try with a different search or consider
+                 contributing to Beacon’s database after you next go shopping so that other
+                  shoppers can access this information.</h1>
             </div>
         );
     }
