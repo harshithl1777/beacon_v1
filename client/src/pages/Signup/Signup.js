84ccb8d1136943e13d1f22f330f3c398c83691c3
Harshith Latchupatula <harshithl1777@gmail.com>
2021-03-23 12:20:06 -0400

    changed login format

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -35,6 +35,7 @@ class Signup extends Component {
                 })
                 .then(({ data }) => {
                     console.log(data.uid);
+                    window.location.href='/app/search';
                 })
                 .catch(({ result }) => {
                     console.log(result);
@@ -112,20 +113,6 @@ class Signup extends Component {
                             <button onClick={() => this.getStartedClicked()} className="sign-up__btn btn--sea">Get Started with Beacon</button>
                         </div>
                         <img className='sign-up__seperator' src={line} alt='Line' draggable='false' />
-                        <div className='sign-up__third-party'>
-                            <button onClick={() => this.providerLogin('google')} className='sign-up__google'>
-                                <div className='sign-up__google-content'>
-                                    <img src={googleIcon} alt='google' className='sign-up__google-icon' />
-                                    Start with Google
-                                </div>
-                            </button>
-                            <button onClick={() => this.providerLogin('fb')} className='sign-up__fb'>
-                                <div className='sign-up__fb-content'>
-                                    <img src={fbIcon} alt='facebook' className='sign-up__fb-icon' />
-                                    Start with Facebook
-                                </div>
-                            </button>
-                        </div>
                         
                 </div>
             </div>
